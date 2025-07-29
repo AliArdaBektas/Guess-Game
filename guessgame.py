1da57dcb01bf74d719d1de4796121ced04dad3f5
Ali Arda <aliardabektas2@gmail.com>
2025-07-30 00:34:04 +0300

    guess limits and guess counter has been added

--- guessgame.py
+++ guessgame.py
@@ -2,17 +2,21 @@ import random
 number = random.randint(1,100)
 
 guess = int(input("guess a number 1 to 100: "))
-guess_right = 1
-
+guess_count = 1
+guess_limit = 10
 while guess != number:
+    guess_limit -= 1
+    if guess_limit == 0:
+        print("you lost " + str(guess_count) + " times tried")
+        break
     if guess > number:
-        print("lower than guess")
+        print("higher than number, you have " + str(guess_limit) + " guess left:")
 
     elif guess < number:
-        print("higher than guess")
+        print("lower than number, you have " + str(guess_limit) + " guess left:")
 
     guess = int(input("try to guess it again: "))
 
-    guess_right += 1
-
-print(f"congrats, {guess_right} times tried to guess")
+    guess_count += 1
+else:
+    print(f"congrats, {guess_count} times tried to guess")
